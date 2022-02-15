d1bfdd6601e4465d9558506a2f61b8f6c131bd0b
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-15 00:05:44 -0600

    Array of stars filled using conditional

--- src/RatingSystem.js
+++ src/RatingSystem.js
@@ -1,23 +1,30 @@
 import React, { useState } from 'react'
-import fiveStarImg from './FiveStar';
-import FilledStar from './assets/filled-star.svg'
-import EmptyStar from './assets/empty-star.svg';
+// import fiveStarImg from './FiveStar';
+// import FilledStar from './assets/filled-star.svg'
+// import EmptyStar from './assets/empty-star.svg';
 
 
 const RatingSystem = ( ) => {
+  // the state is initialized at the first index
     const [rating, setRating] = useState(0)
 
-  return (
+    return (
     <div>
       <h1>5 star rating system</h1>
       <h2>Select a rating:</h2>
-        {fiveStarImg.map((star, index) => {
-        index += 1
-        return <button key={index} 
-          onClick={() => (index <= rating ? FilledStar : EmptyStar)
-            // setRating(index)
+        {/* created an array containing 5 indexes */}
+        {[...Array(5)].map((star,index) => {
+          return <button key={index} 
+        /* If the indices of the stars are less or equal to the star that is clicked, 
+        all those stars fitting that criteria will fill. ie: click the 4th star
+        (all star indices < or = to the index of the 4th star fill. 
+        The 5th star wouldn't fill. It's index is > than that of the forth) */
+          className={index <= rating ? "filled-star" : "unfilled-star"}
+        /* set the state of the rating to the index of the clicked star */
+          onClick={() => (setRating(index))
           }>
-            <img key={index} src={star} alt={"empty-star"}/>
+          {/* HTML code for star */}
+            <span className="star">&#9733;</span> 
           </button>
         
         })}
