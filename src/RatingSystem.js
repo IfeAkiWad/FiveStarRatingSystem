41480064d9900e0119dd49cc418280d60f014883
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-17 17:12:03 -0600

    add ClothingComponent & <h1> & <p>

--- src/RatingSystem.js
+++ src/RatingSystem.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import ClothingComponent from './ClothingComponent'
 
 const RatingSystem = ( ) => {
   // the state is initialized at the first index
@@ -7,35 +8,34 @@ const RatingSystem = ( ) => {
 
     return (
     <div>
-      <h1>5 star rating system</h1>
-      <h2>Select a rating:</h2>
-        {/* created an array containing an array length of 5 */}
-        {[...Array(5)].map((star,index) => {
-          return <button key={index} 
-        /* If the indices of the stars are less or equal to the star that is clicked of hovered over, 
-        all those stars fitting that criteria will fill. ie: click/hover over the 4th star
-        (all star indices < or = to the index of the 4th star fill. 
-        The 5th star wouldn't fill. It's index is > than that of the forth) */
-          className={index <= (hover || rating) ? "filled-star" : "unfilled-star"}
-        /* set the state of the rating to the index of the clicked star */
-          onClick={() => (setRating(index))}
-        /* double clicking a star will reset the rating */
-          onDoubleClick={() => (setRating(!index))}
-        /* hovering over stars */
-          onMouseOver={() => setHover(index) }
-          onMouseLeave={() => setHover(rating)}
-          >
-          {/* HTML code for star */}
-            <span className="star">&#9733;</span> 
-          </button>
-        
-        })}
+      <h1 className="rating-heading">How did you enjoy your purchase?</h1>
+      <p>Please let us know by giving this purchase a rating.</p>
+      <ClothingComponent />
+        <h2 className="rating-heading-2">Select a rating:</h2>
+        <div className="star-center">
+          {/* created an array containing an array length of 5 */}
+          {[...Array(5)].map((star,index) => {
+              return <button key={index} 
+            /* If the indices of the stars are less or equal to the star that is clicked or hovered over, 
+            all those stars fitting that criteria will fill. ie: click/hover over the 4th star
+            (all star indices < or = to the index of the 4th star fill. 
+            The 5th star wouldn't fill. Its index is > than that of the forth) */
+              className={index <= (hover || rating) ? "filled-star" : "unfilled-star"}
+            /* set the state of the rating to the index of the clicked star */
+              onClick={() => (setRating(index))}
+            /* double clicking a star will reset the rating */
+              onDoubleClick={() => (setRating(!index))}
+            /* hovering over stars */
+              onMouseOver={() => setHover(index) }
+              onMouseLeave={() => setHover(rating)}
+              >
+              {/* HTML code for star */}
+                <span className="star">&#9733;</span> 
+              </button>
+          })}
+      </div>
     </div>
   )
-    // if (double clicked) {
-//    setRating(!index)
-    // }
-  
 }
 
 export default RatingSystem
