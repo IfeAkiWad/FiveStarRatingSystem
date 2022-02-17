0f90d990fba8489b4715ef0dbc5049d45b92e539
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-17 17:22:41 -0600

    import NavBar .js and BrowserRouter, Router, Routes, and Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RatingSystem from './RatingSystem';
+import NavBar from './NavBar';
 import './styles.css';
 
 export default function App() {
   return (
     <div className="App">
-      <RatingSystem />
+       <Router>
+                <NavBar />
+                    <Routes>
+                    <Route path="/" element={<RatingSystem />} /> 
+                    {/* <Route path="/clickers" element={<UseStateHook />}/>
+                    <Route path="/age-calculator" element={<AgeCalculator />}/>
+                    <Route path="/timer" element={<Timer />}/>
+                    <Route path="/clock" element={<Clock />}/>
+                    <Route path="/vacation" element={<Vacation />}/> */}
+                    </Routes>
+        </Router>
     </div>
   )
 };
