cc2ce11c5e9c21d08d0a630d0b45b78a47cb8541
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-17 17:25:24 -0600

    create NavBar.js for navigation

---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <nav className="nav">
+            {/* <button class="dropbtn">Menu</button> */}
+            <div>  
+                <Link id="link" to='/'> HOME</Link> 
+                {/* <Link id="link" to="/vacation"> VACATION</Link> 
+                <Link id="link" to="/clickers"> CLICKERS</Link> 
+                <Link id="link" to="/age-calculator"> AGE CALCULATOR</Link> 
+                <Link id="link" to='/timer'> TIMER</Link> 
+                <Link id="link" to='/clock'> CLOCK</Link> */}
+            </div>
+    </nav>
+    )
+}
+
+export default NavBar
