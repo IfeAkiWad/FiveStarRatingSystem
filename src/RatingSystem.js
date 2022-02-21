6cd5b2405f50caa47807157fb2b1bf6d2a0dfcbd
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-21 14:20:36 -0600

    Refactor state to be null on initialization

--- src/RatingSystem.js
+++ src/RatingSystem.js
@@ -3,8 +3,8 @@ import ClothingComponent from './ClothingComponent'
 
 const RatingSystem = ( ) => {
   // the state is initialized at the first index
-    const [rating, setRating] = useState(0)
-    const [hover, setHover] = useState(0)
+    const [rating, setRating] = useState(null)
+    const [hover, setHover] = useState(null)
 
     return (
     <div>
@@ -15,16 +15,17 @@ const RatingSystem = ( ) => {
         <div className="star-center">
           {/* created an array containing an array length of 5 */}
           {[...Array(5)].map((star,index) => {
+            let indexValue = index += 1
               return <button key={index} 
             /* If the indices of the stars are less or equal to the star that is clicked or hovered over, 
             all those stars fitting that criteria will fill. ie: click/hover over the 4th star
             (all star indices < or = to the index of the 4th star fill. 
             The 5th star wouldn't fill. Its index is > than that of the forth) */
-              className={index <= (hover || rating) ? "filled-star" : "unfilled-star"}
+              className={indexValue <= (hover || rating) ? "filled-star" : "unfilled-star"}
             /* set the state of the rating to the index of the clicked star */
-              onClick={() => (setRating(index))}
+              onClick={() => (setRating(indexValue))}
             /* double clicking a star will reset the rating */
-              onDoubleClick={() => (setRating(!index))}
+              onDoubleClick={() => (setRating(!indexValue))}
             /* hovering over stars */
               onMouseOver={() => setHover(index) }
               onMouseLeave={() => setHover(rating)}
