c56d23894d63f6837393c3a64dfbc371e4563965
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-21 19:02:45 -0600

    create Footer functional component

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <footer>2022 Built and Designed by Ifeoluwa Akinremi</footer>
+    </div>
+  )
+}
+export default Footer
